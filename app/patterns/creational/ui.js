052ee493b87480ab2371f99a285b020f7ba22e8f
Roman Novozhilov <romannovojilov@mail.ru>
2019-02-04 10:01:39 +0300

    ui added

---
+++ app/patterns/creational/ui.js
@@ -0,0 +1,40 @@
+//factory method
+
+
+"use strict";
+(function () {
+    var namespace = App.define("patterns.creational.ui");
+
+    function Control() {
+
+    }
+    
+    Control.prototype.render = function(type) {
+        return "Control: " + this.name + " <br /> " + this.control + " <br /><br />";
+    }
+    
+    //factory method
+    Control.create = function(type) {
+        if( typeof Control[type] !== "function")
+            throw {
+                name: "Error",
+                message: "Constructor " + type + " not found."
+            };
+    
+        if(!(Control[type].prototype instanceof Control))
+            Control[type].prototype = new Control();
+    
+        return new Control[type]();
+    }
+    
+    Control.Button = function() {
+        this.name = "Button";
+        this.control = "<input type='button' value='" + this.name + "' />";
+    }
+    Control.Text = function() {
+        this.name = "Text";
+        this.control = "<input type='text' value='' placeholder='" + this.name + "' />";
+    }
+    
+    namespace.Control = Control;
+})();
